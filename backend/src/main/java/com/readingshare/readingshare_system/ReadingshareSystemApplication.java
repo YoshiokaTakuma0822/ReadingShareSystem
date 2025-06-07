a7935792fd1e6fdfca1f0fc1a8d0768020ed5f1d
DaigoEnomoto <al23064@shibaura-it.ac.jp>
2025-06-07 11:01:22 +0900

    backendファイルを追加

---
+++ backend/src/main/java/com/readingshare/readingshare_system/ReadingshareSystemApplication.java
@@ -0,0 +1,13 @@
+package com.readingshare.readingshare_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingshareSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingshareSystemApplication.class, args);
+	}
+
+}
