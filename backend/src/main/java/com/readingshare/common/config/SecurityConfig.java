d18f3498fa4b84cb2131739b79723ce22add6d3a
al23002 <207835823+al23002@users.noreply.github.com>
2025-06-17 14:03:48 +0900

    Create SecurityConfig.java

---
+++ backend/src/main/java/com/readingshare/common/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.readingshare.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authz -> authz.anyRequest().permitAll())
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+
+        return http.build();
+    }
+}
