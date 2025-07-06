e22d843fd614c45c478b2c038fe7fcff5a9b3a74
al23002 <207835823+al23002@users.noreply.github.com>
2025-07-06 21:56:03 +0900

    Update HomeScreen.tsx

--- frontend/app/screens/HomeScreen.tsx
+++ frontend/app/screens/HomeScreen.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from 'react'
 import AuthGuard from '../../components/AuthGuard'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../../components/ui/card'
 import { HoverPopover } from '../../components/ui/my-hover-popover'
 import { getDummyUserId, logout } from '../../lib/authUtils'
 import { roomApi } from '../../lib/roomApi'
@@ -255,20 +254,6 @@ const HomeScreen: React.FC = () => {
                 <div style={{ marginBottom: 32 }}>
                     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 8 }}>
                         <h1 style={{ color: 'var(--green-dark)', fontSize: 32, margin: 0 }}>読書共有システム</h1>
-
-                        {/* Card単体テスト */}
-                        <div style={{ position: 'absolute', top: 100, right: 32, zIndex: 1000 }}>
-                            <Card className="w-64 border-0 shadow-none">
-                                <CardHeader>
-                                    <CardTitle className="text-lg">テストCard</CardTitle>
-                                    <CardDescription>border-0テスト</CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    <div>テスト内容</div>
-                                </CardContent>
-                            </Card>
-                        </div>
-
                         {/* ユーザープロフィール表示 */}
                         <HoverPopover
                             align="end"
