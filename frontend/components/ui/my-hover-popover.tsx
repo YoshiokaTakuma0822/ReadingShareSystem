29b239b2a5dd8df5bec3e3235cef3515124caafd
al23002 <207835823+al23002@users.noreply.github.com>
2025-07-06 21:54:43 +0900

    Revert "Update my-hover-popover.tsx"
    This reverts commit b22dbb2b363b087d103822d324b89c56ee00e048.

--- frontend/components/ui/my-hover-popover.tsx
+++ frontend/components/ui/my-hover-popover.tsx
@@ -152,7 +152,7 @@ export function HoverPopover({
                     onMouseEnter={handleContentMouseEnter}
                     onMouseLeave={handleContentMouseLeave}
                     className={cn(
-                        "bg-white text-gray-900 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 rounded-md shadow-lg outline-hidden",
+                        "bg-white text-gray-900 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 rounded-md border border-gray-200 shadow-lg outline-hidden",
                         // ピン留め状態の視覚的フィードバック
                         isPinned && "ring-2 ring-blue-500 ring-opacity-20",
                         className
