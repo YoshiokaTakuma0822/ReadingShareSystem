8f854d7a1005fe0780f54c241d1e7b0dec3529a4
al23002 <207835823+al23002@users.noreply.github.com>
2025-07-06 12:36:07 +0900

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
             },
             {
                 source: '/internal/:path*',
-                destination: 'http://localhost:8888/:path*',
+                destination: 'http://host.docker.internal:8888/:path*',
             },
         ]
     },
